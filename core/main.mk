6e9d40877169e97741716643904986be0f31ab51
snak3ater <saeedyasir91@gmail.com>
2015-04-27 13:42:15 +0500

    Revert "main: WITH_DEXPREOPT := false"
    This reverts commit 9b0de01fb40ca519445b0eda583958595689b80b.
    
    Revert "more WITH_DEXPREOPT flag"
    
    This reverts commit 998c3260ee14bc3c6365dac18c3c2df37b50e86c.

--- core/main.mk
+++ core/main.mk
@@ -315,7 +315,6 @@ endif
 
 user_variant := $(filter user userdebug,$(TARGET_BUILD_VARIANT))
 enable_target_debugging := true
-WITH_DEXPREOPT := false
 tags_to_install :=
 ifneq (,$(user_variant))
   # Target is secure in user builds.
@@ -339,7 +338,7 @@ ifneq (,$(user_variant))
     ifeq ($(DALVIK_VM_LIB),libdvm.so)
       ifeq ($(user_variant),user)
         ifeq ($(HOST_OS),linux)
-          WITH_DEXPREOPT := false
+          WITH_DEXPREOPT := true
         endif
       endif
     endif
@@ -371,7 +370,6 @@ endif # !enable_target_debugging
 
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 tags_to_install := debug eng
-WITH_DEXPREOPT := false
 ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))),)
   # Don't require the setup wizard on eng builds
   ADDITIONAL_BUILD_PROPERTIES := $(filter-out ro.setupwizard.mode=%,\
